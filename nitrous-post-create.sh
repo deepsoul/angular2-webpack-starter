ceefd3d64078aa56cc114b528ec470bf634e82e4
Boris Horn <horn.boris@gmail.com>
2016-07-12 16:48:20 +0200

    Create nitrous-post-create.sh

---
+++ nitrous-post-create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'Installing required NPM global packages...'
+npm install typings webpack-dev-server rimraf webpack -g --no-progress
+echo 'Installing NPM packages...'
+npm install --no-progress
